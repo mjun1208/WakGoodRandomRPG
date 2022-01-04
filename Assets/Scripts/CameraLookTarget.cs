f7a8c85a413d23a33dc0a96bbedc8e5184f3268b
minjun <mjun1208@naver.com>
2022-01-04 23:06:55 +0900

    CameraLookTarget 추가

---
+++ Assets/Scripts/CameraLookTarget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Management.Instrumentation;
+using UnityEngine;
+
+public class CameraLookTarget : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    
+    private void Update()
+    {
+        this.transform.position = _target.transform.position + new Vector3(0f, 1.5f, 0f);
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouse_x = Input.GetAxis("Mouse X");
+        float mouse_y = Input.GetAxis("Mouse Y");
+        
+        this.transform.rotation *= Quaternion.AngleAxis(mouse_x, Vector3.up);
+        this.transform.rotation *= Quaternion.AngleAxis(-mouse_y * 0.2f, Vector3.right);
+    }
+}
