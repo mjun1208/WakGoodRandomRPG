cca624936999ec9718663041c981cb4fc797357b
minjun <mjun1208@naver.com>
2022-01-04 23:07:40 +0900

    Constant, Enumeration 추가

---
+++ Assets/Scripts/Enumeration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ActorStats
+{
+    STR, // 힘
+    DEX, // 재주
+    CON, // 건강
+    INT, // 지능
+    WIS, // 지혜
+    CHA // 매력
+}
