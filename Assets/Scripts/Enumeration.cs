785e6ff2a9b0bffb09f0746f77703b88493fad64
minjun <mjun1208@naver.com>
2022-01-08 04:46:33 +0900

    NameSpace 추가

--- Assets/Scripts/Enumeration.cs
+++ Assets/Scripts/Enumeration.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ActorStats
+namespace WRR
 {
-    STR, // 힘
-    DEX, // 재주
-    CON, // 건강
-    INT, // 지능
-    WIS, // 지혜
-    CHA // 매력
+    public enum ActorStats
+    {
+        STR, // 힘
+        DEX, // 재주
+        CON, // 건강
+        INT, // 지능
+        WIS, // 지혜
+        CHA // 매력
+    }
 }
