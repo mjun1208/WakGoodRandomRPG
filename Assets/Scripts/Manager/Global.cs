ee0e35cb05f0f83cec366866e081ab020fcb0f0d
minjun <mjun1208@naver.com>
2022-01-16 22:36:01 +0900

    게임 총괄하는 Global 추가, UI 관리하는 UIManager 추가

---
+++ Assets/Scripts/Manager/Global.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WRR
+{
+    public class Global : MonoBehaviour
+    {
+        public static Global Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    return null;
+                }
+
+                return _instance;
+            }
+        }
+        
+        public static UIManager UIManager => _uiManager;
+        
+        private static Global _instance;
+        private static UIManager _uiManager;
+
+        public static Actor MyActor { get; private set; }
+
+        private void Awake()
+        {
+            _instance = this;
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WRR
+{
+    public class UIManager : MonoBehaviour
+    {
+    }
+}
