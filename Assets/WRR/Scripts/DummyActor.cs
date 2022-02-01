7ddc713e555b2d72a0d87cf49b67f30c3456fa28
minjun <mjun1208@naver.com>
2022-02-02 04:34:20 +0900

    더미 Actor 추가

---
+++ Assets/WRR/Scripts/DummyActor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyActor : MonoBehaviour
+{
+    public void SetPosition(Vector3 position, float rotation)
+    {
+        this.transform.position = position;
+        
+        float rotationVelocity = 0f;
+        rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref rotationVelocity, 0f);
+        this.transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+    }
+}
