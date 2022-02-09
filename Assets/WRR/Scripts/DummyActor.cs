8bcfd5ad96640a10930aa79646b49112b620d07a
김민준 <mjun1208@meerkatgames.co.kr>
2022-02-09 12:18:15 +0900

    더미 이동 개선
    움직이는 속도 일정하게, 중간값은 계산 안하고 결과값만 계산

--- Assets/WRR/Scripts/DummyActor.cs
+++ Assets/WRR/Scripts/DummyActor.cs
@@ -3,18 +3,30 @@
 public class DummyActor : MonoBehaviour
 {
     private Vector3 targetPosition;
-    
+    private float _lastRotation;
+
     public void Update()
     {
-        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition,5f * Time.deltaTime);
+        float distance = Vector3.Distance(targetPosition, this.transform.position);
+        if (distance > 0.05f)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, 5f * Time.deltaTime);
+
+            var dir = targetPosition - this.transform.position;
+            
+            float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+            this.transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        }
+        else
+        {
+            this.transform.position = targetPosition;
+            this.transform.rotation = Quaternion.Slerp( this.transform.rotation, Quaternion.Euler(0.0f, _lastRotation, 0.0f), 15f * Time.deltaTime);
+        }
     }
 
     public void SetPosition(Vector3 position, float rotation)
     {
         targetPosition = position;
-        
-        float rotationVelocity = 0f;
-        rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref rotationVelocity, 0f);
-        this.transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        _lastRotation = rotation;
     }
 }
